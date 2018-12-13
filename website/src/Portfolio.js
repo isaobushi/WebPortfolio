350e020c61ee7e6952eaf6450bf64ce8580d4c96
Andrea Mele <andrea.mele.au@gmail.com>
2018-12-13 14:35:47 +1100

    portfolio

--- website/src/Portfolio.js
+++ website/src/Portfolio.js
@@ -1,16 +1,16 @@
 import React from 'react'
-import './Tile'
+import Tile from './Tile'
 
 
 export default class Portfolio extends React.Component {
   render() {
     return (
-      <div className='grid'>
-      <Tile />
-      <Tile />
-      <Tile />
-      <Tile />
-      </div>
+        <div className='grid'>
+          <Tile />
+          <Tile />
+          <Tile />
+          <Tile />
+        </div>
     )
   }
 }
--- website/src/Tile.js
+++ website/src/Tile.js
@@ -1,11 +1,41 @@
 import React from 'react'
 
 export default class Tile extends React.Component{
-  render(){
+  constructor(){
+    super()
+    this.state = {
+      active: false,
+      clicked: false
+    }
+  }
+
+  toggleActive() {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+    render(){
 
     return(
 
-           <div></div>
+    
+      <div 
+      className="tile" 
+      style={{
+        backgroundColor: this.state.active ? 'green' : 'yellow',
+        width: this.state.active ? '30rem' : '15rem',
+        height: this.state.active ? '30rem' : '15rem',
+        position: this.state.active ? 'fixed' : '',
+        top: this.state.active ? '0px' : '',
+        left: this.state.active ? '0px' : '',
+        transform: this.state.active ? 'translate(90%, 20%)' : '',
+        margin: this.state.active ? 'auto' : ''
+
+        // top: this.state.active ? '0' : '',
+       }}
+        onClick={() => this.toggleActive()}
+      >
+      </div>
     )
 
   }
