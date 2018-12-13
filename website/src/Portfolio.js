6dd318169cb0feca1861d8e003c1cf8a50ec6f29
Andrea Mele <andrea.mele.au@gmail.com>
2018-12-13 17:18:17 +1100

    developing portfolio pictures

--- website/src/Portfolio.js
+++ website/src/Portfolio.js
@@ -1,16 +1,33 @@
 import React from 'react'
 import Tile from './Tile'
+import BackgroundImageTTT from './ttt.png'; // Relative Path to BackgroundImage
+import BackgroundImageEAF from './eaf.png'; // Relative Path to BackgroundImage
+import BackgroundImagePortfolio from './website.png'; // Relative Path to BackgroundImage
+
+
 
 
 export default class Portfolio extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+      projects: [
+        {title: 'TTT', image: BackgroundImageTTT, stats: 'no'},
+        {title: 'EAF', image: BackgroundImageEAF, stats: 'yes'},
+        {title: 'Portfolio', image: BackgroundImagePortfolio, stats: 'soso'}
+      ]
+    }
+  }
+
+
   render() {
     return (
-        <div className='grid'>
-          <Tile />
-          <Tile />
-          <Tile />
-          <Tile />
-        </div>
+      <div className='grid'>
+          {this.state.projects.map(project=>{
+            // console.log(project.title)
+           return <Tile projects = {{title: project.title, image: project.image, stats: project.stats }} />
+          })}
+      </div>
     )
   }
 }
--- website/src/Tile.js
+++ website/src/Tile.js
@@ -1,11 +1,14 @@
 import React from 'react'
 
 export default class Tile extends React.Component{
-  constructor(){
+  constructor(props){
     super()
+    console.log(props.projects.image)
     this.state = {
       active: false,
-      clicked: false
+      title: props.projects.title,
+      backgroundImage: props.projects.image,
+      footer: props.projects.stats
     }
   }
 
@@ -14,27 +17,31 @@ export default class Tile extends React.Component{
       active: !this.state.active
     })
   }
+  
     render(){
-
-    return(
-
-    
-      <div 
-      className="tile" 
-      style={{
-        backgroundColor: this.state.active ? 'green' : 'yellow',
+      
+      let styleTile = {
+        // backgroundColor: this.state.active ? 'green' : 'tranparent',
         width: this.state.active ? '30rem' : '15rem',
         height: this.state.active ? '30rem' : '15rem',
         position: this.state.active ? 'fixed' : '',
         top: this.state.active ? '0px' : '',
         left: this.state.active ? '0px' : '',
         transform: this.state.active ? 'translate(90%, 20%)' : '',
-        margin: this.state.active ? 'auto' : ''
-
-        // top: this.state.active ? '0' : '',
-       }}
+        margin: this.state.active ? 'auto' : '',
+        backgroundImage: `url(${this.state.backgroundImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center'
+       }
+    return(
+      
+      <div 
+      className="tile"
+      style={styleTile}
         onClick={() => this.toggleActive()}
       >
+        <div className="tile-title">{this.state.title}</div>
+        <div className="tile-stats">{this.state.footer}</div>
       </div>
     )
 
